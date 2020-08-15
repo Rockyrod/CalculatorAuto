8f074834c27fe5fe2cb614e392f0a9921f5c3eab
Rockyrod <vikashprasad001@gmail.com>
2020-08-15 21:25:53 +0530

    Added functions for add and sub

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""
+Calcualor Auto containig math calculations
+
+"""
+
+def add(first_item, second_item):
+    return first_item + second_item
+
+def sub(first_item,second_item):
+    return first_item - second_item
+
